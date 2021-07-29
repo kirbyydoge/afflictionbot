9f3fe966368d76199b478e5064e69f4eef6a7831
kirbyydoge <aqwoguz@gmail.com>
2021-07-29 12:06:54 +0300

    Debug Info

--- bot.py
+++ bot.py
@@ -164,6 +164,7 @@ async def help(ctx:commands.Context, *arg):
 @client.event
 async def on_ready():
 	global youtube, youtube_channel, cached_videos, cached_youtubers
+    print("Setting Up")
 	youtube = build('youtube', 'v3', developerKey=GOOGLE_DEV_KEY)
 	youtube_channel_id = bdb.youtube_channel_load()
 	if youtube_channel_id:
@@ -174,6 +175,7 @@ async def on_ready():
 		cached_videos = {}
 	update_cache()
 	youtube_check.start()
+    print("Ready To Roll")
 
 @tasks.loop(seconds=60)
 async def youtube_check():
